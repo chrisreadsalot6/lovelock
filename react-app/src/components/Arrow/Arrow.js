0edece1d646762797df83e7ed16c329931f39178
Chris Read <24418909+rcreadii@users.noreply.github.com>
2021-02-10 20:01:28 -0500

    add left icon

--- react-app/src/components/Arrow/Arrow.js
+++ react-app/src/components/Arrow/Arrow.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Arrow() {
   return (
     <div>
-      <i className="long arrow alternative right icon">Turn to the right!</i>
+      <i>Turn to the right!</i>
+      <span className="long arrow alternative right icon" />
+      <i className="long arrow alternative light icon">Turn to the left!</i>
+      <span className="long arrow alternative left icon" />
     </div>
   );
 }
