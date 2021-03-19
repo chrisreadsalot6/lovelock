38cf6921b205c08f17e2e0540a82fb6402a18b75
Chris Read <24418909+rcreadii@users.noreply.github.com>
2021-03-19 11:08:03 -0400

    sqlalchemy fix

--- app/config.py
+++ app/config.py
@@ -2,6 +2,9 @@ import os
 
 class Config:
   SECRET_KEY=os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+  if os.environ.get("FLASK_ENV") == "production":
+    SQLALCHEMY_DATABASE_URI=postgres://xoiysdhgjdzdqm:ef18c6e02087abdc3e4034dd46ae767a533ba740e0394c636a80d125fe7367b4@ec2-54-211-176-156.compute-1.amazonaws.com:5432/daq8gsr4vabhhv
+  else:
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
   SQLALCHEMY_ECHO=True
   SQLALCHEMY_TRACK_MODIFICATIONS=False
